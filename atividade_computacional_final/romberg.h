e0de6fbbcca11239a6da3d2324428aecbc4aa0be
Felipe Matheus Oliveira Silva <76018796+fe94matheus@users.noreply.github.com>
2023-03-10 13:27:50 -0300

    Delete atividade_computacional_final directory

--- atividade_computacional_final/romberg.h
+++
@@ -1,7 +0,0 @@
-
-
-double romb(double (*f)(double), double a, double b);    
-double romb_2(double (*f)(double, double), double a, double b, double alpha);
-double romb_3(double (*f)(double, double, double), double a, double b, double alpha, double beta);
-
-
